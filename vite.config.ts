b3bf07d045b1e490f97f4c06a686241b92860a30
aldorusso <83636449+aldorusso@users.noreply.github.com>
2025-04-25 14:52:45 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -73,6 +73,10 @@ const gitInfo = getGitInfo();
 
 export default defineConfig((config) => {
   return {
+    server: {
+      host: true,
+      allowedHosts: ['magictools-bolt.ldfjte.easypanel.host']
+    },
     define: {
       __COMMIT_HASH: JSON.stringify(gitInfo.commitHash),
       __GIT_BRANCH: JSON.stringify(gitInfo.branch),
